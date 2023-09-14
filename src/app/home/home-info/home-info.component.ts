3e93320750cebb3e74a404d49946c35f98fc7f3f
mflanders06 <mflanders06@gmail.com>
2023-09-14 15:43:18 -0600

    weather api working/displaying info

--- src/app/home/home-info/home-info.component.ts
+++ src/app/home/home-info/home-info.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeServiceService } from 'src/app/services/home-service.service';
+import { Subscription } from 'rxjs';
+import {
+  HomeServiceService,
+  CurrentWeather,
+  WeatherImport,
+} from 'src/app/services/home-service.service';
 
 @Component({
   selector: 'app-home-info',
@@ -7,13 +12,45 @@ import { HomeServiceService } from 'src/app/services/home-service.service';
   styleUrls: ['./home-info.component.scss'],
 })
 export class HomeInfoComponent implements OnInit {
-  weatherJson = {};
+  weatherSubscription = Subscription;
+
+  todayWeather: CurrentWeather = {
+    is_day: true,
+    temperature: 0,
+    time: '',
+    weathercode: 0,
+    winddirection: 0,
+    windspeed: 0,
+  };
+
+  weatherJson: WeatherImport = {
+    current_weather: this.todayWeather,
+    elevation: 0,
+    generationtime_ms: 0,
+    latitude: 0,
+    longitude: 0,
+    timezone: '',
+    timezone_abbreviation: '',
+    utc_offset_seconds: 0,
+  };
 
   constructor(private homeService: HomeServiceService) {}
 
   ngOnInit(): void {
-    this.homeService.getData().subscribe((weatherData) => {
-      console.log(weatherData);
+    this.pullWeatherData();
+  }
+
+  // ngOnDestroy(): void {
+  //   if (this.weatherSubscription) {
+  //     this.weatherSubscription.unsubscribe();
+  //   }
+  // }
+
+  pullWeatherData() {
+    // this.weatherSubscription =
+    this.homeService.getData().subscribe((weatherData: WeatherImport) => {
+      this.weatherJson = weatherData;
+      console.log(this.weatherJson.current_weather);
     });
   }
 }
--- src/app/services/home-service.service.ts
+++ src/app/services/home-service.service.ts
@@ -12,7 +12,26 @@ export class HomeServiceService {
     return this.http.get(
       'https://api.open-meteo.com/v1/forecast?latitude=40.41&longitude=111.88&temperature_unit=fahrenheit&current_weather=true'
     );
+    // Lat = 40.410283648176744, Lon = -111.88816705409161
   }
 }
 
-// Lat = 40.410283648176744, Lon = -111.88816705409161
+export interface WeatherImport {
+  current_weather: CurrentWeather;
+  elevation: number;
+  generationtime_ms: number;
+  latitude: number;
+  longitude: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  utc_offset_seconds: number;
+}
+
+export interface CurrentWeather {
+  is_day: boolean;
+  temperature: number;
+  time: string;
+  weathercode: number;
+  winddirection: number;
+  windspeed: number;
+}
